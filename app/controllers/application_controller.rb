64eca0da9e73b05f4204ad4708cb5cc4e64aa1a5
Mehreen Butt <mbutt@republiccharterschools.org>
2018-07-01 10:46:40 +0000

    bar feutz

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,13 @@ class ApplicationController < Sinatra::Base
     return erb :index
   end
 
+  get '/frontswipe'  do
+    return erb :frontswipe
+  end
+  
   post '/' do
+    @name = params[:name]
+    @classroom = params[:room]
     return erb :submit
   end
 end
